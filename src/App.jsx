de04568494e3405a16ca341df264f5b4dd5fea47
wondersmasher <wonderfulakinniyi@gmai.com>
2023-04-27 20:47:26 +0100

    Changed Images with removed background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Container } from "@mui/material";
 import NavBar from "./components/NavBar";
+import Trails from "./components/Trials";
+import Carousel from "./components/Carousel";
 function App() {
   return (
     <Container maxWidth="off">
       <NavBar />
+      <Trails />
+      <Carousel />
     </Container>
   );
 }
--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,58 +1,96 @@
-// import "../assets/imageProject/shirts/";
-// import "../assets/imageProject/chairs/1.jpg";
-import shirtImg1 from "../assets/imageProject/shirts/photo-1.jpg";
-
-import shirtIimg2 from "../assets/imageProject/shirts/2.jpg";
-import shirtIimg3 from "../assets/imageProject/shirts/3.jpg";
-import shirtIimg4 from "../assets/imageProject/shirts/4.jpg";
-import shirtImg5 from "../assets/imageProject/shirts/5.jpg";
-import shirtIimg6 from "../assets/imageProject/shirts/6.jpg";
-import shirtImg7 from "../assets/imageProject/shirts/7.jpg";
-import shirtImg8 from "../assets/imageProject/shirts/8.jpg";
-import ShirtImg9 from "../assets/imageProject/shirts/9.jpg";
-import shirtImg10 from "../assets/imageProject/shirts/10.jpg";
-import shirtImg11 from "../assets/imageProject/shirts/11.jpg";
-import shirtImg12 from "../assets/imageProject/shirts/12.jpg";
-import shirtImg13 from "../assets/imageProject/shirts/13.jpg";
+import bagImg1 from "../assets/images/bags/1.png";
+import bagImg2 from "../assets/images/bags/2.png";
+import bagImg3 from "../assets/images/bags/3.png";
+import bagImg4 from "../assets/images/bags/4.png";
+import bagImg5 from "../assets/images/bags/5.png";
+import bagImg6 from "../assets/images/bags/6.png";
+import bagImg7 from "../assets/images/bags/7.png";
+import bagImg8 from "../assets/images/bags/8.png";
+import bagImg9 from "../assets/images/bags/9.png";
+import bagImg10 from "../assets/images/bags/10.png";
+import bagImg11 from "../assets/images/bags/11.png";
+import bagImg12 from "../assets/images/bags/12.png";
+import bagImg13 from "../assets/images/bags/13.png";
+import bagImg14 from "../assets/images/bags/14.png";
+import bagImg15 from "../assets/images/bags/15.png";
+import bagImg16 from "../assets/images/bags/16.png";
+// import chair1 from "../assets/images/chairs/1.png";
+// import chair2 from "../assets/images/chairs/2.png";
+// import chair3 from "../assets/images/chairs/3.png";
+// import chair4 from "../assets/images/chairs/4.png";
+// import chair5 from "../assets/images/chairs/5.png";
+// import chair6 from "../assets/images/chairs/6.png";
+// import chair7 from "../assets/images/chairs/7.png";
+// import chair8 from "../assets/images/chairs/8.png";
+// import chair9 from "../assets/images/chairs/9.png";
+// import chair10 from "../assets/images/chairs/10.png";
+// import femaleShoe1 from "../assets/images/femaleShoes/1.png";
+// import femaleShoe2 from "../assets/images/femaleShoes/2.png";
+// import femaleShoe3 from "../assets/images/femaleShoes/3.png";
+// import femaleShoe4 from "../assets/images/femaleShoes/4.png";
+// import femaleShoe5 from "../assets/images/femaleShoes/5.png";
+// import headphone1 from "../assets/images/headphones/1.png";
+// import headphone2 from "../assets/images/headphones/2.png";
+// import headphone3 from "../assets/images/headphones/3.png";
+// import headphone4 from "../assets/images/headphones/4.png";
+// import headphone5 from "../assets/images/headphones/5.png";
+// import headphone6 from "../assets/images/headphones/6.png";
+// import headphone7 from "../assets/images/headphones/7.png";
+// import menShoe1 from "../assets/images/menShoes/1.png";
+// import menShoe2 from "../assets/images/menShoes/1.png";
+// import menShoe3 from "../assets/images/menShoes/1.png";
+// import menShoe4 from "../assets/images/menShoes/1.png";
+// import menShoe5 from "../assets/images/menShoes/1.png";
+// import menShoe6 from "../assets/images/menShoes/1.png";
+// import menShoe7 from "../assets/images/menShoes/1.png";
+// import menShoe8 from "../assets/images/menShoes/1.png";
+// import menShoe9 from "../assets/images/menShoes/1.png";
+// import menShoe10 from "../assets/images/menShoes/1.png";
+// import menShoe11 from "../assets/images/menShoes/1.png";
+// import menShoe12 from "../assets/images/menShoes/1.png";
+// import menShoe13 from "../assets/images/menShoes/1.png";
+// import menShoe14 from "../assets/images/menShoes/1.png";
+// import menShoe15 from "../assets/images/menShoes/1.png";
+// import menShoe16 from "../assets/images/menShoes/1.png";
 const projectData = [
   {
     id: 1,
-    title: "Striped Shirt",
+    title: "Bag 1",
     price: 109.95,
     description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: shirtImg1,
+    category: "Bags",
+    image: bagImg1,
     rating: { rate: 3.9, count: 120 },
   },
   {
     id: 2,
-    title: "Original T-Shirts ",
+    title: "Bag 2 ",
     price: 22.3,
     description:
       "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-    category: "men's clothing",
-    image: shirtIimg2,
+    category: "Bags",
+    image: bagImg2,
     rating: { rate: 4.1, count: 259 },
   },
   {
     id: 3,
-    title: "White T-shirt",
+    title: "Bag 3",
     price: 55.99,
     description:
       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-    category: "men's clothing",
-    image: shirtIimg3,
+    category: "Bags",
+    image: bagImg3,
     rating: { rate: 4.7, count: 500 },
   },
   {
     id: 4,
-    title: "Blue Jacket",
+    title: "Bag 4",
     price: 15.99,
     description:
       "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-    category: "men's clothing",
-    image: shirtIimg4,
+    category: "Bags",
+    image: bagImg4,
     rating: { rate: 2.1, count: 430 },
   },
   {
@@ -61,8 +99,8 @@ const projectData = [
     price: 695,
     description:
       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-    category: "men's clothing",
-    image: shirtImg5,
+    category: "Bags",
+    image: bagImg5,
     rating: { rate: 4.6, count: 400 },
   },
   {
@@ -71,8 +109,8 @@ const projectData = [
     price: 168,
     description:
       "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-    category: "men's clothing",
-    image: shirtIimg6,
+    category: "Bags",
+    image: bagImg6,
     rating: { rate: 3.9, count: 70 },
   },
   {
@@ -81,8 +119,8 @@ const projectData = [
     price: 9.99,
     description:
       "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-    category: "men's clothing",
-    image: shirtImg7,
+    category: "Bags",
+    image: bagImg7,
     rating: { rate: 3, count: 400 },
   },
   {
@@ -91,8 +129,8 @@ const projectData = [
     price: 10.99,
     description:
       "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
-    category: "men's clothing",
-    image: shirtImg8,
+    category: "Bags",
+    image: bagImg8,
     rating: { rate: 1.9, count: 100 },
   },
   {
@@ -101,8 +139,8 @@ const projectData = [
     price: 64,
     description:
       "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-    category: "men's clothing",
-    image: ShirtImg9,
+    category: "Bags",
+    image: bagImg9,
     rating: { rate: 3.3, count: 203 },
   },
   {
@@ -111,8 +149,8 @@ const projectData = [
     price: 109,
     description:
       "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
-    category: "men's clothing",
-    image: shirtImg10,
+    category: "Bags",
+    image: bagImg10,
     rating: { rate: 2.9, count: 470 },
   },
   {
@@ -121,8 +159,8 @@ const projectData = [
     price: 109,
     description:
       "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
-    category: "men's clothing",
-    image: shirtImg11,
+    category: "Bags",
+    image: bagImg11,
     rating: { rate: 4.8, count: 319 },
   },
   {
@@ -131,8 +169,8 @@ const projectData = [
     price: 114,
     description:
       "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
-    category: "men's clothing",
-    image: shirtImg12,
+    category: "Bags",
+    image: bagImg12,
     rating: { rate: 4.8, count: 400 },
   },
   {
@@ -141,87 +179,40 @@ const projectData = [
     price: 599,
     description:
       "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
-    category: "men's clothing",
-    image: shirtImg13,
+    category: "Bags",
+    image: bagImg13,
     rating: { rate: 2.9, count: 250 },
   },
-  // {
-  //   id: 14,
-  //   title:
-  //     "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
-  //   price: 999.99,
-  //   description:
-  //     "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
-  //   category: "men's shoes",
-  //   image: "../assets/imageProject/maleShoes/1.jpg",
-  //   rating: { rate: 2.2, count: 140 },
-  // },
-  // {
-  //   id: 15,
-  //   title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
-  //   price: 56.99,
-  //   description:
-  //     "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-  //   category: "men's shoes",
-
-  //   image: "../assets/imageProject/maleShoes/2.jpg",
-  //   rating: { rate: 2.6, count: 235 },
-  // },
-  // {
-  //   id: 16,
-  //   title:
-  //     "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
-  //   price: 29.95,
-  //   description:
-  //     "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
-  //   category: "men's shoes",
-
-  //   image: "../assets/imageProject/maleShoes/3.jpg",
-  //   rating: { rate: 2.9, count: 340 },
-  // },
-  // {
-  //   id: 17,
-  //   title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
-  //   price: 39.99,
-  //   description:
-  //     "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
-  //   category: "men's shoes",
-  //   image: "../assets/imageProject/maleShoes/4.jpg",
-  //   rating: { rate: 3.8, count: 679 },
-  // },
-  // {
-  //   id: 18,
-  //   title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
-  //   price: 9.85,
-  //   description:
-  //     "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
-  //   category: "men's shoes",
-
-  //   image: "../assets/imageProject/maleShoes/5.jpg",
-  //   rating: { rate: 4.7, count: 130 },
-  // },
-  // {
-  //   id: 19,
-  //   title: "Opna Women's Short Sleeve Moisture",
-  //   price: 7.95,
-  //   description:
-  //     "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
-  //   category: "men's shoes",
-
-  //   image: "../assets/imageProject/maleShoes/6.jpg",
-  //   rating: { rate: 4.5, count: 146 },
-  // },
-  // {
-  //   id: 20,
-  //   title: "DANVOUY Womens T Shirt Casual Cotton Short",
-  //   price: 12.99,
-  //   description:
-  //     "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-  //   category: "men's shoes",
-
-  //   image: "../assets/imageProject/maleShoes/7.jpg",
-  //   rating: { rate: 3.6, count: 145 },
-  // },
+  {
+    id: 14,
+    title: "Sky Blue Jeans Jacket",
+    price: 599,
+    description:
+      "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
+    category: "Bags",
+    image: bagImg14,
+    rating: { rate: 2.9, count: 250 },
+  },
+  {
+    id: 15,
+    title: "Sky Blue Jeans Jacket",
+    price: 599,
+    description:
+      "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
+    category: "Bags",
+    image: bagImg15,
+    rating: { rate: 2.9, count: 250 },
+  },
+  {
+    id: 16,
+    title: "Sky Blue Jeans Jacket",
+    price: 599,
+    description:
+      "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
+    category: "Bags",
+    image: bagImg16,
+    rating: { rate: 2.9, count: 250 },
+  },
 ];
 
 export default projectData;
