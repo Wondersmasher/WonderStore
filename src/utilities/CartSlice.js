fcc5a402c6b1cdadfc6055a87a5ce67266deff89
wondersmasher <wonderfulakinniyi@gmai.com>
2023-05-06 10:57:49 +0100

    added more reducer functions for cart and favouite

--- src/utilities/CartSlice.js
+++ src/utilities/CartSlice.js
@@ -4,30 +4,61 @@ const initialState = {
   // projectData,
   cartItems: [],
   cartItemsCount: 0,
-  favouritItems: [],
+  favouriteItems: [],
   favouriteItemsCount: 0,
-  gridOrFlex: false,
+  gridOrFlex: true,
 };
 const CartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    try: (state) => console.log(state),
     addCartCount: (state, { payload }) => {
       state.cartItemsCount += payload;
     },
-    addFavoutiteCount: (state) => {
-      state.favouriteItemsCount + 1;
-    },
     setGridOrFlexTrueOrFalse: (state, { payload }) => {
       state.gridOrFlex = payload;
+    },
+    addToCartItem: (state, { payload }) => {
+      const tempItem = state.cartItems.find((item) => item.id === payload.id);
+      if (tempItem) {
+        state.cartItems.map((item) => {
+          if (item.id === payload.id) {
+            item.amount += payload.amount;
+          }
+        });
+      } else {
+        const newItem = { ...payload };
+        state.cartItems = [...state.cartItems, newItem];
+      }
+    },
+    toggleFavoriteItem: (state, { payload }) => {
+      const tempFavourite = state.favouriteItems.find(
+        (item) => item.id === payload.id
+      );
+      if (tempFavourite) {
+        const newFavouriteItems = state.favouriteItems.filter(
+          (item) => item.id !== payload.id
+        );
+        state.favouriteItems = newFavouriteItems;
+        state.favouriteItemsCount -= 1;
+      } else {
+        const newFavorites = { ...payload };
+        state.favouriteItems = [...state.favouriteItems, newFavorites];
+        state.favouriteItemsCount += 1;
+      }
+    },
+    removeFromCartItem: (state, { payload }) => {
+      const removedTheItem = state.cartItems.filter(
+        (item) => item.id !== payload.id
+      );
+      state.cartItems = removedTheItem;
     },
   },
 });
 export const {
   addCartCount,
-  setGridOrFlexFalse,
   setGridOrFlexTrueOrFalse,
-  addFavoutiteCount,
+  addToCartItem,
+  toggleFavoriteItem,
 } = CartSlice.actions;
 export default CartSlice.reducer;
