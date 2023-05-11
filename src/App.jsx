cae30cdab5bb5b8aa6cb99a00a0b60093137e2ed
wondersmasher <wonderfulakinniyi@gmai.com>
2023-05-11 21:52:54 +0100

    Addtheme, also want 2 implemnt laz load. commented

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,21 @@ import AboutUsPage from "./pages/AboutUsPage";
 import Shop from "./pages/Shop";
 import SingleProductPage from "./pages/SingleProductPage";
 import CartPage from "./pages/CartPage";
+// import { Suspense, lazy } from "react";
+// const SingleProductPage = lazy(() => import("./pages/SingleProductPage"));
+// const Shop = lazy(() => import("./pages/shop"));
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#C5A491",
+    },
+  },
+  typography: {
+    fontFamily: "Dancing",
+  },
+});
 function App() {
   return (
     <BrowserRouter>
@@ -12,8 +27,22 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
           <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:productId" element={<SingleProductPage />} />
+          <Route
+            path="/shop"
+            element={
+              // <Suspense fallback={<div>Loading...</div>}>
+              <Shop />
+              // </Suspense>
+            }
+          />
+          <Route
+            path="/shop/:productId"
+            element={
+              // <Suspense fallback={<div>Loading</div>}>
+              <SingleProductPage />
+              // </Suspense>
+            }
+          />
           <Route path="/cart" element={<CartPage />} />
         </Route>
         <Route path="*" element={<div>Error... Something went wrong</div>} />
