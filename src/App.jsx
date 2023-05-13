12c15c1231e7d7991694b6506add57a087e88f33
wondersmasher <wonderfulakinniyi@gmai.com>
2023-05-13 20:46:06 +0100

    Removed theme, createtheme, suspense and lazy

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,7 @@ import AboutUsPage from "./pages/AboutUsPage";
 import Shop from "./pages/Shop";
 import SingleProductPage from "./pages/SingleProductPage";
 import CartPage from "./pages/CartPage";
-// import { Suspense, lazy } from "react";
-// const SingleProductPage = lazy(() => import("./pages/SingleProductPage"));
-// const Shop = lazy(() => import("./pages/shop"));
-import { ThemeProvider, createTheme } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#C5A491",
-    },
-  },
-  typography: {
-    fontFamily: "Dancing",
-  },
-});
 function App() {
   return (
     <BrowserRouter>
@@ -27,22 +13,8 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
           <Route path="/about-us" element={<AboutUsPage />} />
-          <Route
-            path="/shop"
-            element={
-              // <Suspense fallback={<div>Loading...</div>}>
-              <Shop />
-              // </Suspense>
-            }
-          />
-          <Route
-            path="/shop/:productId"
-            element={
-              // <Suspense fallback={<div>Loading</div>}>
-              <SingleProductPage />
-              // </Suspense>
-            }
-          />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:productId" element={<SingleProductPage />} />
           <Route path="/cart" element={<CartPage />} />
         </Route>
         <Route path="*" element={<div>Error... Something went wrong</div>} />
